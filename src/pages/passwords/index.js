50e3f512e52c5c9ac02e58ab90c5d4eefd8c982f
GustavoGuerraFerreira <gutvasco123@gmail.com>
2024-02-15 17:42:40 -0300

    generatePassword

---
+++ src/pages/passwords/index.js
@@ -0,0 +1,12 @@
+import{View, Text, StyleSheet} from "react-native"
+
+export function Passowords(){
+    return(
+        <View>
+            <Text>Minhas senhas</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+
+})
